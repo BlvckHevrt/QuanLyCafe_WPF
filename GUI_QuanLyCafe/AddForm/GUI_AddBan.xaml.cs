b1d2355c48e1af06f597067a7c304fe509acda24
BlvckHevrt <156943971+BlvckHevrt@users.noreply.github.com>
2024-08-31 18:57:46 +0700

    Thêm các form quản lý còn lại

---
+++ GUI_QuanLyCafe/AddForm/GUI_AddBan.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_QuanLyCafe.AddForm
+{
+    /// <summary>
+    /// Interaction logic for GUI_AddBan.xaml
+    /// </summary>
+    public partial class GUI_AddBan : Window
+    {
+        public GUI_AddBan()
+        {
+            InitializeComponent();
+        }
+    }
+}
